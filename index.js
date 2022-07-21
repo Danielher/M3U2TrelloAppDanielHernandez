bc67a5cbf14666b9de5212d3bad8805ea72fca03
Danyel <dahaj.1@gmail.com>
2022-07-21 18:31:41 -0500

    se crea Index JS y se mejora html

---
+++ index.js
@@ -0,0 +1,9 @@
+function mostrar(){
+    var formulario = document.getElementById('seccionForm');
+    formulario.style.display = "block";
+}
+
+function ocultar(){
+    var formulario = document.getElementById('seccionForm');
+    formulario.style.display = "none";
+}
