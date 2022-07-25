aa66b77aa25a7990b9d88658358d3c70f438131f
Danyel <dahaj.1@gmail.com>
2022-07-25 18:55:12 -0500

    iniciailizando librerias

--- index.js
+++ index.js
@@ -7,3 +7,25 @@ function ocultar(){
     var formulario = document.getElementById('seccionForm');
     formulario.style.display = "none";
 }
+
+document.getElementById('register-form').addEventListener('submit', guardaTarea);
+
+function guardaTarea (e) {
+
+    let titulo = document.getElementById("titulo").value;
+    let descripcion = document.getElementById("Descripcion").value;
+    let responsable = document.getElementById("Responsable").value;
+    let fechaEnt = document.getElementById("FechaEn").value;
+
+    const tarea = {
+        titulo,
+        descripcion,
+        responsable,
+        fechaEnt
+    };
+
+    console.log(tarea)
+
+    e.preventDefault();
+
+}
---
+++ js/api/requests.js
@@ -0,0 +1,3 @@
+// Creación de varible para guarda la URL de la API Fake
+const API_URL = "https://my-json-server.typicode.com/https://github.com/Danielher/M3U2TrelloAppDanielHernandez";
+
